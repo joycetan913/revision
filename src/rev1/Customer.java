2cffe8acfb7eb51add8ce484764ff407f7e8df07
joycetan913 <joycetanyy913@gmail.com>
2024-10-29 22:31:28 +0800

    initial commit

---
+++ src/rev1/Customer.java
@@ -0,0 +1,54 @@
+package rev1;
+
+public class Customer {
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String company;
+    private String city;
+    private String country;
+    private String phone1;
+    private String phone2;
+    private String email;
+    private String subscriptionDate;
+    private String website;
+
+    // Constructor
+    public Customer(String customerId, String firstName, String lastName, String company, String city,
+                    String country, String phone1, String phone2, String email,
+                    String subscriptionDate, String website) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.company = company;
+        this.city = city;
+        this.country = country;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.email = email;
+        this.subscriptionDate = subscriptionDate;
+        this.website = website;
+    }
+
+    // Getter for customerId
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", company='" + company + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", phone1='" + phone1 + '\'' +
+                ", phone2='" + phone2 + '\'' +
+                ", email='" + email + '\'' +
+                ", subscriptionDate='" + subscriptionDate + '\'' +
+                ", website='" + website + '\'' +
+                '}';
+    }
+}
--- src/rev1/readCSV.java
+++ src/rev1/readCSV.java
@@ -4,37 +4,54 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class readCSV {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FileNotFoundException{
         
         String inputFile = args[0];
         String outputFile = "output.txt";
-        List<String[]> data = new ArrayList<>();
+
+        Map<String, Customer> customerMap = new HashMap<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-            String line = "x";
-            while ((line = reader.readLine()) != null) 
-                String[] row = line.split(",");
-                data.add(row);
-        
-            if (line == null)
-                break;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
 
-        reader.close();
+                if (values.length < 12) {
+                    System.out.println("Skipping incomplete line: " + line);
+                    continue; // Skip incomplete lines
+                }
+
+                Customer customer = new Customer(
+                        values[1],  // Customer Id
+                        values[2],  // First Name
+                        values[3],  // Last Name
+                        values[4],  // Company
+                        values[5],  // City
+                        values[6],  // Country
+                        values[7],  // Phone 1
+                        values[8],  // Phone 2
+                        values[9],  // Email
+                        values[10], // Subscription Date
+                        values[11]  // Website
+                );
+
+                customerMap.putIfAbsent(customer.getCustomerId(), customer);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
         }
-        Map<String, AppRating> appRatings = new HashMap<>();
-        
-        for (String[] row: data) 
-            String appName = row[0];
-            String category = row[1];
-            double rating = row[2]; 
-        
 
-
-            
-        
+        // Example of retrieving a customer
+        Customer c = customerMap.get("9F9AdB7B8A6f7F2");
+        if (c != null) {
+            System.out.println(c.toString());
+        } else {
+            System.out.println("Customer not found.");
+        }
     }
-
 }
